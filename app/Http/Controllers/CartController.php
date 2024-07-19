4fcee10f85d8b2dbb628531ea160cc3c8a2e4f9a
noeldesalegn <desalegnbekele2121@gmail.com>
2024-07-19 20:04:41 +0300

    Delete And Clear Cart Item

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -24,6 +24,17 @@ class CartController extends Controller
         Cart::instance('cart')->update($request->rowId,$request->quantity);
         return redirect()->route('cart.index');
     }
+    public function removeItem(Request $request)
+    {
+        $rowId = $request->rowId;
+        Cart::instance('cart')->remove($rowId);
+        return redirect()->route('cart.index');
+    }
+    public function clearCart()
+    {
+        Cart::instance('cart')->destroy();
+        return redirect()->route('cart.index');
+    }
 }
 
 
